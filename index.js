79574bb4cc1c9d8793127a861a4fc2a45bd75d34
KarolWoz <111453950+KarolWoz@users.noreply.github.com>
2022-12-18 21:25:26 +0100

    naniesione poprawki

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const sel = document.querySelector("#currencyOption");
 const input = document.querySelector("#currencyAmount");
 const btn = document.querySelector("#currencyCalc");
 const calculatedValue = document.querySelector("#calculatedValue");
+const exchangeForm=document.querySelector('#exchangeForm')
 
 const getCurrencyList = () => {
   fetch(urlAll)
     .then((response) => response.json())
-    .then((data) => getRates(data[0].rates))
+    .then((data) => getCalc(data[0].rates))
     .catch((err) => console.log(err));
 };
 
-const getRates = (rates) => {
-  const selectValue = sel.value;
-  const mid = rates.find((rate) => rate.code === selectValue).mid;
-  const calculation = mid * input.value;
-  calculatedValue.innerText = calculation.toFixed(2);
+const getCalc = (rates) => {
+  if (input.value > 0) {
+    const selectValue = sel.value;
+    const mid = rates.find((rate) => rate.code === selectValue).mid;
+    const calculation = mid * input.value;
+    calculatedValue.innerText = calculation.toFixed(2);
+  }
 };
 
 btn.addEventListener("click", () => {
